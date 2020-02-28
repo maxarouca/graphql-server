9d34b1177b5449b9b94ccd8de59f1523b066a620
Maximiler Arouca <maximiler@gmail.com>
2020-02-28 16:34:57 -0300

    Remove Old Files

--- src/schemas/query.js
+++ src/schemas/query.js
@@ -2,10 +2,7 @@ import { gql } from 'apollo-server'
 
 const Query = gql`
   type Query {
-    ola: String!
-    horaAtual: Date!
     loggedUser: User
-    product: Product
     users: [User]
     user(id: Int): User
     profiles: [Profile]
